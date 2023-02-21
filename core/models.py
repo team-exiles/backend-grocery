c0d00f814bf9ac46b057770ec9d56e5588a9b3dc
Jacob Gilbert <jacobgilb03@gmail.com>
2023-02-21 10:30:42 -0500

    User profile and lists.

--- core/models.py
+++ core/models.py
@@ -3,4 +3,16 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return self.username
+
+class ItemList(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ItemLists')
+    title = models.CharField(max_length=50)
+    check_box = models.BooleanField(default=False)
+    item = models.CharField(max_length=50)
+    quantity = models.CharField(max_length=50, null=True)
+    # archived = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title}'
--- core/views.py
+++ core/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from .models import User, ItemList
+from rest_framework.generics import ListCreateAPIView, ListAPIView
+from .serializers import UserSerializer, ItemListSerializer
 # Create your views here.
+
+class UserView(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        queryset = User.objects.filter(username=self.request.user)
+        return queryset
+    
+class ItemListsList(ListAPIView):
+    queryset = ItemList.objects.all()
+    serializer_class = ItemListSerializer
+
+class MyLists(ListCreateAPIView):
+    serializer_class = ItemListSerializer
+
+    def get_queryset(self):
+        return ItemList.objects.filter(owner=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user) 
+
