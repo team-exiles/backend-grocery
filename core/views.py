feaccce96032b249b76f6ac6739592aad70bcffb
Jacob Gilbert <jacobgilb03@gmail.com>
2023-02-22 13:42:12 -0500

    views

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import User, ItemList
-from rest_framework.generics import ListCreateAPIView, ListAPIView
+from rest_framework.generics import ListCreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import UserSerializer, ItemListSerializer
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class UserView(ListCreateAPIView):
@@ -25,3 +26,12 @@ class MyLists(ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user) 
 
+class ListDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = ItemListSerializer
+    lookup_url_kwarg = 'list_id'
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.method == 'GET':
+            return ItemList.objects.all()
+        return ItemList.objects.filter(owner=self.request.user)
