637f5d0c70d58912014554bd85af45b8c49e73af
Jacob Gilbert <jacobgilb03@gmail.com>
2023-02-24 14:32:44 -0500

    Very messed up

--- core/models.py
+++ core/models.py
@@ -19,7 +19,7 @@ class User(AbstractUser):
 
 class ItemList(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ItemLists')
-    title = models.CharField(max_length=50, unique=True)
+    title = models.CharField(max_length=50)
     # items_for_list = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='ListItems', null=True)
     # created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     # archived = models.BooleanField(default=False)
@@ -33,13 +33,6 @@ class Item(models.Model):
     item = models.CharField(max_length=50)
     check_box = models.BooleanField(default=False)
     # quantity = models.CharField(max_length=50, null=True)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-            fields=['list_for_items', 'item'], name='unique_item'
-            )
-        ]
 
     def __str__(self):
         return f'{self.item}'
