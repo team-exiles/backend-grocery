025734256806d093b2b19f74e59f6748695c9be1
Jacob Gilbert <jacobgilb03@gmail.com>
2023-02-25 17:15:36 -0500

    Debug

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
