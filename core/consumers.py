57d86640e986030cd4b85747c03ece7252e1269c
Jacob Gilbert <jacobgilb03@gmail.com>
2023-03-05 17:53:09 -0500

    WebSocket fix?

--- core/consumers.py
+++ core/consumers.py
@@ -1,7 +1,8 @@
 import json
 from asgiref.sync import async_to_sync, sync_to_async
-from channels.generic.websocket import AsyncWebsocketConsumer, JsonWebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Item, User, ItemList
+from channels.db import database_sync_to_async
 # from .serializers import MessageSerializer
 
 # myapp/consumers.py
@@ -31,26 +32,48 @@ class ListConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+    @database_sync_to_async
+    def get_item(self, item_id):
+        return Item.objects.get(id=item_id)
+
+    @database_sync_to_async
+    def get_or_create_list(self, itemlist_id):
+        return ItemList.objects.get_or_create(id=itemlist_id)
+
+    @database_sync_to_async
+    def create_item(self, list_obj, item_text):
+        return Item.objects.create(list_for_items=list_obj, item=item_text)
+
     async def receive(self, text_data):
         data = json.loads(text_data)
         item_id = data.get('item_id')
 
         if item_id is not None:
             item = await sync_to_async(Item.objects.get)(id=item_id)
-            list_obj = await sync_to_async(ItemList.objects.get_or_create)(id=self.itemlist_id)
-            
+            list_obj, _ = await sync_to_async(ItemList.objects.get_or_create)(id=self.itemlist_id)
 
-        # Add the item to the list and save it
-            new_item = await sync_to_async(Item.objects.create)(list_for_items=list_obj[0], item=item.item)
+            # Add the item to the list and save it
+            new_item = await self.create_item(list_obj, item.item)
 
-        # Send a message to the list group with the updated list
+            # Save the new item to the database
+            await self.save_item(new_item)
+
+            # Send a message to the list group with the updated list
             await self.channel_layer.group_send(
                 self.itemlist_group_name,
                 {
                     'type': 'list.update',
-                    'list': await sync_to_async(list_obj[0].to_json)()
+                    'list': await self.list_to_json(list_obj),
                 }
             )
+
+    @database_sync_to_async
+    def save_item(self, item):
+        item.save()
+
+    @database_sync_to_async
+    def list_to_json(self, list_obj):
+        return list_obj.to_json()
 
     async def list_update(self, event):
         # Send the updated list to the client
